b635d3cff3ef5132fac73e7cd0aa97cf712e309a
Kevin Malakoff <kmalakoff@gmail.com>
2022-07-10 02:00:30 -0700

    Upgrade tests to get rid of @testing-library

--- test/lib/visit.ts
+++
@@ -1,7 +0,0 @@
-import { TestInstance } from 'react-test-renderer';
-type Visitor = (element: TestInstance) => void;
-
-export default function visit(element: TestInstance, fn: Visitor) {
-  fn(element);
-  element.children?.forEach((x) => visit(x, fn));
-}
