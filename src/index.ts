7ab4235522d93bb64870a2c696c92126c8f4a66b
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-21 04:36:23 -0800

    Upgrade modules

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { useState, useEffect, useContext, createContext, createElement } from 'react';
+import { createContext, createElement, useContext, useEffect, useState } from 'react';
 import type { ReactNode } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import type { GestureResponderEvent } from 'react-native';
 
 export type EventTypes = GestureResponderEvent;
@@ -52,5 +52,6 @@ export function useEvent(handler, dependencies) {
     throw new Error('react-native-event: subscribe not found on context. You might be missing the EventProvider or have multiple instances of react-native-event');
   }
 
+  // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
   useEffect(() => context.subscribe(handler), [context.subscribe, handler].concat(dependencies));
 }
