79b5def8d644619d9ccbc3003a823db51693af76
Kevin Malakoff <kmalakoff@gmail.com>
2024-12-26 20:00:11 -0800

    Upgrade modules

--- test/exports/import.test-rn.ts
+++ test/exports/import.test-rn.ts
@@ -1,8 +1,9 @@
 import assert from 'assert';
-import { EventContext, useEvent, EventProvider } from 'react-native-event';
+// @ts-ignore
+import { EventContext, EventProvider, useEvent } from 'react-native-event';
 
-describe('exports .mjs', function () {
-  it('defaults', function () {
+describe('exports .mjs', () => {
+  it('defaults', () => {
     assert.equal(typeof EventContext, 'object');
     assert.equal(typeof EventProvider, 'function');
     assert.equal(typeof useEvent, 'function');
