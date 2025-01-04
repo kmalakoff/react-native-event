6c7c2c4a92ab7741c64b8b9ea4b544d0da789bbe
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-03 22:21:31 -0800

    0.2.6

--- test/exports/import.test.mjs
+++ test/exports/import.test.mjs
@@ -1,12 +1,10 @@
-// SKIP: esm import not support by react-native
+import assert from 'assert';
+import { EventContext, EventProvider, useEvent } from 'react-native-event';
 
-// import assert from 'assert';
-// import { EventContext, useEvent, EventProvider } from 'react-native-event';
-
-describe('exports .ts', function () {
-  it('defaults', function () {
-    // assert.equal(typeof EventContext, 'object');
-    // assert.equal(typeof EventProvider, 'function');
-    // assert.equal(typeof useEvent, 'function');
+describe('exports .ts', () => {
+  it('defaults', () => {
+    assert.equal(typeof EventContext, 'object');
+    assert.equal(typeof EventProvider, 'function');
+    assert.equal(typeof useEvent, 'function');
   });
 });
--- test/exports/require.test.cjs
+++ test/exports/require.test.cjs
@@ -1,8 +1,8 @@
 const assert = require('assert');
 const { EventContext, useEvent, EventProvider } = require('react-native-event');
 
-describe('exports .ts', function () {
-  it('defaults', function () {
+describe('exports .cjs', () => {
+  it('defaults', () => {
     assert.equal(typeof EventContext, 'object');
     assert.equal(typeof EventProvider, 'function');
     assert.equal(typeof useEvent, 'function');
