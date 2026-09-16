025c0965e9446cb7e8229f88839805b83bc510a3
Kevin Malakoff <kmalakoff@gmail.com>
2026-09-16 09:13:34 -0700

    Use explicit CocoaPods installation for the iOS fixture

--- examples/native-test-app/react-native.config.js
+++ examples/native-test-app/react-native.config.js
@@ -1,29 +1,13 @@
-const project = (() => {
-  try {
-    const { configureProjects } = require("react-native-test-app");
-    return configureProjects({
-      android: {
-        sourceDir: "android",
-      },
-      ios: {
-        sourceDir: "ios",
-      },
-      macos: {
-        sourceDir: "macos",
-      },
-      visionos: {
-        sourceDir: "visionos",
-      },
-      windows: {
-        sourceDir: "windows",
-        solutionFile: "windows/EventSmoke.sln",
-      },
-    });
-  } catch (_) {
-    return undefined;
-  }
-})();
+const { configureProjects } = require('react-native-test-app');
 
 module.exports = {
-  ...(project ? { project } : undefined),
+  project: configureProjects({
+    android: {
+      sourceDir: 'android',
+    },
+    ios: {
+      sourceDir: 'ios',
+      automaticPodsInstallation: false,
+    },
+  }),
 };
